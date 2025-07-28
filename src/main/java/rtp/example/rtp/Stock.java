3f6b6da0b6835835ff9e4139cc3cbcb5cc46ad36
benrichie <s3849557@student.rmit.edu.au>
2025-07-28 21:41:54 +0200

    feat(entity): add Stock JPA entity with fields and annotations

---
+++ src/main/java/rtp/example/rtp/Stock.java
@@ -0,0 +1,81 @@
+package rtp.example.rtp;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "stocks")
+public class Stock {
+
+    @Id
+    private String symbol; // "AAPL", "GOOGL"
+
+    @Column(nullable = false)
+    private String companyName;
+
+    @Column(nullable = false, precision = 19, scale =4 )
+    private BigDecimal currentPrice; // NEVER Double for money!
+
+    @Column(nullable = false)
+    private LocalDateTime lastUpdated;
+
+    //No-arg contrustor(JPA requirement)
+    public Stock() {}
+
+    public Stock(String symbol, String companyname, BigDecimal currentPrice){
+        this.symbol = symbol;
+        this.companyName = companyname;
+        this.currentPrice = currentPrice;
+        this.lastUpdated = LocalDateTime.now();
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public BigDecimal getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(BigDecimal currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public LocalDateTime getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(LocalDateTime lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Stock)) return false;
+        Stock stock = (Stock) o;
+        return Objects.equals(symbol, stock.symbol);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(symbol);
+    }
+
+
+
+}
