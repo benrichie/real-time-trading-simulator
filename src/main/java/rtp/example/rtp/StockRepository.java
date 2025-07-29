b92e8b732b0f04db73c88fdd1f0192ed4be0ad35
benrichie <s3849557@student.rmit.edu.au>
2025-07-29 11:29:36 +0200

    feat(repository): add StockRepository for Stock entity

---
+++ src/main/java/rtp/example/rtp/StockRepository.java
@@ -0,0 +1,9 @@
+package rtp.example.rtp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+    @Repository
+    public interface StockRepository extends JpaRepository<Stock, String>{
+        // String because symbol (our @Id) is String, not Long
+    }
