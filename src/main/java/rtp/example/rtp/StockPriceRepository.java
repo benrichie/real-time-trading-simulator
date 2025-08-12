fc5d816c6860f6d338519b0ca8dcf2a44ff57af5
benrichie <s3849557@student.rmit.edu.au>
2025-08-12 17:59:09 +0200

    feat: add new query methods to StockPriceRepository

--- src/main/java/rtp/example/rtp/StockPriceRepository.java
+++ src/main/java/rtp/example/rtp/StockPriceRepository.java
@@ -4,7 +4,10 @@ package rtp.example.rtp;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import rtp.example.rtp.Stock.Stock;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +16,10 @@ public interface StockPriceRepository extends JpaRepository<StockPrice, Long> {
     @Query(value = "Select * FROM stock_prices WHERE symbol = ?1 ORDER BY timestamp DESC LIMIT 1", nativeQuery = true)
     Optional<StockPrice> findLatestBySymbol(String symbol);
 
+    @Query("SELECT DISTINCT sp.symbol FROM StockPrice sp")
+    List<String> findAllUniqueSymbols();
+
+    List<StockPrice> findBySymbolAndTimestampAfterOrderByTimestampDesc(String symbol, LocalDateTime after);
+
+    void deleteBySymbolAndTimestampBefore(String symbol, LocalDateTime before);
 }
