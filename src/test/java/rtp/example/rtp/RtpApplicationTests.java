81b06e652e55df6d02459709a25a49ae9ea147d0
benrichie <s3849557@student.rmit.edu.au>
2025-08-13 18:32:25 +0200

    feat: StockPriceRepositoryTest created and annotations changed to test H2(successful)

--- src/test/java/rtp/example/rtp/RtpApplicationTests.java
+++ src/test/java/rtp/example/rtp/RtpApplicationTests.java
@@ -1,9 +1,12 @@
 package rtp.example.rtp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@DataJpaTest
+@ActiveProfiles("test")
 class RtpApplicationTests {
 
 	@Test
