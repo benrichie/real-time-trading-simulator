72697002fa4d2a71f233f95f122e2c3a2c8ff0a0
benrichie <s3849557@student.rmit.edu.au>
2025-08-20 18:39:26 +0200

    chore: doodling how to build RealTimeStockDataService

--- src/main/java/rtp/example/rtp/RealTimeStockDataService.java
+++ src/main/java/rtp/example/rtp/RealTimeStockDataService.java
@@ -1,4 +1,31 @@
 package rtp.example.rtp;
 
 public class RealTimeStockDataService {
+
+    /*
+    * imports stock data from api
+    * communicates live stock price with relevant classes
+    *   which classes are relevant?
+    *       ->update the stock price so maybe stock service
+    *       -> determine exectution price change current price to get live price
+    *
+    *   methods
+    *       getcurrentstockprice - results are cached for 30 seconds
+    *                               - if the price is <30 seconds old, returns it
+    *                               - otherwise calls fetchandupdate
+    *       fetchandupdatestockprice - @transactional
+    *                                - call api to fetch price
+    *                                -validate response
+    *                                   - create stockPrice entity and save, update stock entity current price
+    *
+     *      tracksymbols, stocktracksymbols, getactivesymbols
+     *
+     *      scheludedupdates
+     *
+     *      cleanupoldpricedata
+     *
+     *      dto classes to map json fields from api to java
+     *
+    *
+    * */
 }
