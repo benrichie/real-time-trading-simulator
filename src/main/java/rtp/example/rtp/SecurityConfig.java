6874571b8d61d146cd9064de5ca0c9d2e8fc2ae7
benrichie <s3849557@student.rmit.edu.au>
2025-09-29 18:06:36 +0200

    feat(SecurityConfig): Spring Security configuration defining authentication, authorization rules, and password encoding
     created password encoder

---
+++ src/main/java/rtp/example/rtp/SecurityConfig.java
@@ -0,0 +1,22 @@
+package rtp.example.rtp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+
+public class SecurityConfig {
+
+    public SecurityConfig(){
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
