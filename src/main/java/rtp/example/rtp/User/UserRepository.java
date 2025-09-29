2a2ebafe8e34bd97991c8c11b39da80629a0dfdd
benrichie <s3849557@student.rmit.edu.au>
2025-09-29 18:41:31 +0200

    feat(UserRepository): username/email lookup methods

--- src/main/java/rtp/example/rtp/User/UserRepository.java
+++ src/main/java/rtp/example/rtp/User/UserRepository.java
@@ -3,7 +3,12 @@ package rtp.example.rtp.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> finalByUsername(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
