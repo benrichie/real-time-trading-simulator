b2f00dbd29e0aa27d94bb77693b310f9231703f6
benrichie <s3849557@student.rmit.edu.au>
2025-10-07 18:23:14 +0200

    feat(AuthenticationRequest): DTO for authentication requests

---
+++ src/main/java/rtp/example/rtp/AuthenticationLayer/AuthenticationRequest.java
@@ -0,0 +1,31 @@
+package rtp.example.rtp.AuthenticationLayer;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class AuthenticationRequest {
+    @NotBlank(message = "Password is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    public AuthenticationRequest(){
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
