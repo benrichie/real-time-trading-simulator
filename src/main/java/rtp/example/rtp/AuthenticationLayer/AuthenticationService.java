52d3ceb75c53dd62e4eb8b68de95368660013c4d
benrichie <s3849557@student.rmit.edu.au>
2025-10-10 20:49:09 +0200

    feat():

--- src/main/java/rtp/example/rtp/AuthenticationLayer/AuthenticationService.java
+++ src/main/java/rtp/example/rtp/AuthenticationLayer/AuthenticationService.java
@@ -1,4 +1,22 @@
 package rtp.example.rtp.AuthenticationLayer;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import rtp.example.rtp.JwtInfrastructure.JwtService;
+import rtp.example.rtp.User.UserRepository;
+
 public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+
+    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
 }
