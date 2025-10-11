239ce9668132336a521728384987b689857d37e3
benrichie <s3849557@student.rmit.edu.au>
2025-10-11 19:20:35 +0200

    feat(AuthController):

---
+++ src/main/java/rtp/example/rtp/AuthenticationLayer/AuthController.java
@@ -0,0 +1,30 @@
+package rtp.example.rtp.AuthenticationLayer;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final AuthenticationService authService;
+
+    public AuthController(AuthenticationService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @Valid @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @Valid @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+}
