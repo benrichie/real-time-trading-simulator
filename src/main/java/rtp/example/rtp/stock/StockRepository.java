cf7d4d351952cf82a98db00289fa6d2faa404db3
benrichie <s3849557@student.rmit.edu.au>
2025-10-29 21:13:48 +0100

    feat(TradingService): findBySymbol

--- src/main/java/rtp/example/rtp/stock/StockRepository.java
+++ src/main/java/rtp/example/rtp/stock/StockRepository.java
@@ -3,7 +3,10 @@ package rtp.example.rtp.stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-    @Repository
+import java.util.Optional;
+
+@Repository
     public interface StockRepository extends JpaRepository<Stock, String>{
+        Optional<Stock> findBySymbol(String upperCase);
         // String because symbol (our @Id) is String, not Long
     }
